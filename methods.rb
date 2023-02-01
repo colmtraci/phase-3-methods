f5d0b80f8823ebf441a6326b9f93ccab7f83bc43
Colm Traci <ctraci@me.com>
2023-01-31 19:59:16 -0800

    commit tree df1de3949ad532c8f70f2d627485a338db0ab5e4

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
-# Your code here!
+# --- JS FUNCTION ---
+# function myFunction(param) {
+#   console.log("Running myFunction");
+#   return param + 1;
+# }
+
+# --- RBY METHOD ---
+# def my_method(param)
+#     puts "Running my_method"
+#     param + 1
+# end
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Colm")
+    puts "Hello, #{name}!"
+end
+greet
+
+def greet_with_default(name = "programmer!")
+    puts "Hello, #{name}!"
+end
+greet_with_default
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+    return num / 2
+end
